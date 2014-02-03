64941185bf03fb4d43e2a1e5f5bf47c56aed347b
Rob Rix <rob.rix@github.com>
2014-02-02 22:20:01 -0500

    Add keyTimes for the keyframe animation.

--- WatchCompass/PRHCompassView.m
+++ WatchCompass/PRHCompassView.m
@@ -66,7 +66,9 @@
 		CGPathRelease(sunPath);
 		sunPositionAnimation.rotationMode = kCAAnimationRotateAuto;
 		sunPositionAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
+		sunPositionAnimation.keyTimes = @[ @0, @0.25, @0.5, @0.75, @1 ];
 		//sunPositionAnimation.duration = 24.0 * 3600.0;
+		
 		static const NSTimeInterval dayLength = 5.0;
 		sunPositionAnimation.duration = dayLength; //TEMP
 		sunPositionAnimation.repeatCount = HUGE_VALF;
